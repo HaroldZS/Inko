f268b28f59b3e6bf9067df001b7c7b4efc5bf901
haroldZS <haroldzs1024@gmail.com>
2024-05-05 01:12:08 -0400

    feat: Implement HomePage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { HashRouter, Routes, Route } from "react-router-dom";
 import { Menu } from "./Components/Menu";
+import { HomePage } from "./Components/HomePage";
+import { BlogPage } from "./Components/BlogPage";
+import { LoginPage } from "./Components/LoginPage";
 
 function App() {
   return (
     <>
       <HashRouter>
         <Menu />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/blog" element={<BlogPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
       </HashRouter>
     </>
   );
---
+++ src/Components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="flex justify-center mt-[43px]">
+      <div className="flex w-[216px] h-[65px] bg-[#31363F] rounded-[5px] items-center justify-center">
+        <p className="text-[#EEEEEE] text-[12px]">
+          Welcome to Inko's blogpost!
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export { HomePage };
