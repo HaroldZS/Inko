5c6a062464838dd8d5abc00a34e42c62f9464878
haroldZS <haroldzs1024@gmail.com>
2024-05-07 18:04:21 -0400

    feat: Implement BlogPage

--- src/Components/BlogPage.js
+++ src/Components/BlogPage.js
@@ -1,7 +1,19 @@
 import React from "react";
+import { blogs } from "../data/blogs";
 
 function BlogPage() {
-  return <div>BlogPage</div>;
+  return (
+    <div className="flex justify-center pt-[30px]">
+      <div className="grid grid-cols-1 w-[345px]">
+        {blogs.map((blog) => (
+          <div className="w-auto h-[65px] bg-[#31363F] rounded-[5px] mb-[26px] flex flex-col justify-center pl-[13px]">
+            <p className="text-[#EEEEEE] text-[12px]">{blog.title}</p>
+            <p className="text-[#EEEEEE] text-[10px]">{blog.subTitle}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export { BlogPage };
---
+++ src/data/blogs.js
@@ -0,0 +1,23 @@
+export const blogs = [
+  {
+    title: "The Art of Productivity",
+    subTitle: "Maximizing Your Time and Energy",
+    image:
+      "https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?auto=compress&cs=tinysrgb&w=600",
+    id: 1,
+  },
+  {
+    title: "Exploring the Mind",
+    subTitle: "Strategies for Mental and Emotional Well-being",
+    image:
+      "https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?auto=compress&cs=tinysrgb&w=600",
+    id: 2,
+  },
+  {
+    title: "Purposeful Travel",
+    subTitle: "Discovering Off-the-Beaten-Path Destinations",
+    image:
+      "https://images.pexels.com/photos/4065891/pexels-photo-4065891.jpeg?auto=compress&cs=tinysrgb&w=600",
+    id: 3,
+  },
+];
